fa2a91f6b0bf0504a02b331c4ca45f3f395b3f26
hyejin222 <hyejin-dev@naver.com>
2020-01-31 22:11:19 +0900

    Updates

---
+++ precache-manifest.545d62bdb57e28b9a04765467494fbb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c9ad57c0e83516418570025518484a",
+    "url": "/pf/index.html"
+  },
+  {
+    "revision": "fda4ceb5a550c4bf92f7",
+    "url": "/pf/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3806d9678bf5bc718987",
+    "url": "/pf/static/js/2.02e47eb7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pf/static/js/2.02e47eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda4ceb5a550c4bf92f7",
+    "url": "/pf/static/js/main.112ce182.chunk.js"
+  },
+  {
+    "revision": "8654c075e941e1738d48",
+    "url": "/pf/static/js/runtime-main.d21a6cc4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pf/static/media/logo.5d5d9eef.svg"
+  }
+]);
